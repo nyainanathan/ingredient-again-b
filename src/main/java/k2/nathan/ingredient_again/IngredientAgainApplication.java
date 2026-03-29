a0423a04023334ce7ca794f00661b725305636e3
Ny Aina Nathan A. <nyaina.nathan.1@gmail.com>
2026-03-29 16:54:28 +0300

    Project init

---
+++ src/main/java/k2/nathan/ingredient_again/IngredientAgainApplication.java
@@ -0,0 +1,13 @@
+package k2.nathan.ingredient_again;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngredientAgainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngredientAgainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/k2/nathan/ingredient_again/IngredientAgainApplicationTests.java
@@ -0,0 +1,13 @@
+package k2.nathan.ingredient_again;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngredientAgainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
